8fe651c620f674f2ef128901c65aa8bcfdefc6a2
nagendher <kondanagendher8@gmail.com>
2025-02-19 10:13:59 +0530

    Update 19-Feb-2025

---
+++ void_funstion.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //cout << "nagu"<<endl;
+    cout << "sck ckckc
+            kcmcwlmclwclw
+            lcmlewsclwcwwe"<<endl;
+    return 0;
+}
